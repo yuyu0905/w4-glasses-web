b632ab5cc76be3c71faa33c3edf578cf8d63cb98
amy <amy>
2022-08-03 17:24:05 +0800

    header & footer 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,10 @@
-console.log('Hello!');
-
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+  // menu
+  $("#header-menu-icon").click(function(e) {
+    console.log($(this).siblings('ul'))
+    e.preventDefault();
+    $(this).siblings('ul').slideToggle();
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'maroon': {
+          'light': '#FBF2F2',
+          DEFAULT: '#AA0601',
+          'dark': '#650300'
+        }
+      },
+      screens: {
+        'lg': '1320px',  // => @media (min-width: 1320px) { ... }
+        'md': '720px',   // => @media (min-width: 720px) { ... }
+        'sm': '373px',   // => @media (min-width: 373px) { ... }
+      },
+    },
+    container: {
+      center: true, 
+      padding: "12px"
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
