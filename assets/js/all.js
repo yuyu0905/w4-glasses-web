3937fb947f65b0d436ca201ad561137ee6b4fa53
amy <amy>
2022-08-03 17:29:13 +0800

    Update 2022-08-03T09:28:50.837Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,12 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  console.log('HesSchool Hello!'); // menu
+
+  $("#header-menu-icon").click(function (e) {
+    console.log($(this).siblings('ul'));
+    e.preventDefault();
+    $(this).siblings('ul').slideToggle();
+  });
 });
 //# sourceMappingURL=all.js.map
