565a5e3c084896b3264a01117330dce2a11ff5a6
amy <amy>
2022-08-05 02:41:14 +0800

    Update 2022-08-04T18:41:11.360Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,9 @@
 "use strict";
 
 $(document).ready(function () {
-  console.log('HesSchool Hello!'); // menu
-
+  // menu
   $("#header-menu-icon").click(function (e) {
-    console.log($(this).siblings('ul'));
+    // console.log($(this).siblings('ul'));
     e.preventDefault();
     $(this).siblings('ul').slideToggle();
   });
